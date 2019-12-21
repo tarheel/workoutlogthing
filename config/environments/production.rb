8e9ca57d9a04f833567f56135a98dc333002fcf2
Louis Eisenberg <tarheel@gmail.com>
2019-12-21 12:27:31 -0800

    switch to mysql and install devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { host: 'demo.workoutlogthing.com' }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
